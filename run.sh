5dd19f2c6582d12dffecb5b6fafbc69c95f14dc1
rogerdrag <84144623+rogerdrag@users.noreply.github.com>
2026-06-02 00:00:54 +0300

    добавил bash скрипт

---
+++ run.sh
@@ -0,0 +1,11 @@
+if [ ! -d "inbox" ]; then
+    echo "Ошибка: папка с письмами не найдена"
+    exit 1
+fi
+echo "Запуск приложения"
+python3 -m MailParser
+if [ $? -eq 0 ]; then
+    echo "Приложение успешно завершило работу"
+else
+    echo "Что-то пошло не так"
+fi
